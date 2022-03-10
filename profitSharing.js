b5ea052621d895b6036f01beef86d05e006259fc
Edith-guru <edithnesa@gmail.com>
2022-03-10 17:14:49 +0300

    ways to avoid infinite recursion

--- profitSharing.js
+++ profitSharing.js
@@ -19,3 +19,23 @@ calculatingProfitSharing = (guess) => {
 }
 
 console.log("Gross Profit:\t" + grossProfit + "\nNet Profit:\t" + netProfit + "\nProfit Sharing:\t" + profitSharing);
+
+
+
+//Avoiding infinate recursion
+//Have a break or
+//create a test that ensures the function will stop calling itself after a certain number of calls
+//Can set up a counter that tracks the number of function calls. When the number hits a predetermined maximum, the script should bail out of the recursion process.
+
+// var currentCall = 1;
+// var maximumCall = 3;
+
+// recursionTest();
+
+// function recursionTest(){
+//     if (currentCall <= maximumCall){ 
+//         console.log(currentCall);
+//         currentCall++;
+//         recursionTest();
+//     }
+// }
